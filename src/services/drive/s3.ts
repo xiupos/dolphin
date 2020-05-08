e4fede025e895afea5cc39835da0a2615184fea6
mei23 <m@m544.net>
2020-05-08 10:09:32 +0900

    fix S3

--- src/services/drive/s3.ts
+++ src/services/drive/s3.ts
@@ -1,9 +1,8 @@
 import * as S3 from 'aws-sdk/clients/s3';
 import config from '../../config';
-import { Meta } from '../../models/entities/meta';
 import { getAgentByUrl } from '../../misc/fetch';
 
-export function getS3(meta: Meta) {
+export function getS3() {
 	const u = config.drive.endpoint != null
 		? `${config.drive.useSSL ? 'https://' : 'http://'}${config.drive.endpoint}`
 		: `${config.drive.useSSL ? 'https://' : 'http://'}example.net`;
