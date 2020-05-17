1f90b16ad15b97b828bc0ab83d2735b01e3bcf72
mei23 <m@m544.net>
2020-05-17 12:15:41 +0900

    GC test

--- src/boot/worker.ts
+++ src/boot/worker.ts
@@ -13,6 +13,16 @@ export async function workerMain() {
 	// start job queue
 	require('../queue').default();
 
+	const x = () => {
+		console.log(`MEM: ${JSON.stringify(process.memoryUsage())}`);
+		if (typeof global.gc === 'function') {
+			console.log('GC');
+		} else {
+			console.log('no GC');
+		}
+	}
+	setInterval(x, 1 * 60 * 1000);
+
 	if (cluster.isWorker) {
 		// Send a 'ready' message to parent process
 		process.send!('ready');
