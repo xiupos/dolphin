18acacc14dffadc1c1edc6c642411e0f7d097c91
mei23 <m@m544.net>
2021-10-30 19:42:30 +0900

    リレー向けのActivityが一部実装で除外されてしまうことがあるのを修正

--- src/remote/activitypub/renderer/undo.ts
+++ src/remote/activitypub/renderer/undo.ts
@@ -4,5 +4,6 @@ import { ILocalUser, User } from '../../../models/entities/user';
 export default (object: any, user: ILocalUser | User) => ({
 	type: 'Undo',
 	actor: `${config.url}/users/${user.id}`,
-	object
+	object,
+	published: new Date().toISOString(),
 });
